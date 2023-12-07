3044216a088480d234bbbcebcbb55dbcb7c40a03
Grant Orchard <go@hashicorp.com>
2023-12-07 20:50:47 +1100

    update ldif to add user to group

--- secrets_ldap.tf
+++ secrets_ldap.tf
@@ -3,7 +3,7 @@ resource "vault_ldap_secret_backend" "config" {
   path         = var.ldap_path
   binddn       = var.ldap_binddn
   bindpass     = var.ldap_bindpass
-  url          = "ldaps://hashicorp.local:636"
+  url          = var.ldap_url
   insecure_tls = var.ldap_insecure_tls
   userdn       = var.ldap_userdn
 
