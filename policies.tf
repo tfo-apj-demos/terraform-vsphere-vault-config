94e233fa59811a12a1692067fa80f2c88746ce43
Aaron Evans <aarone@outlook.com.au>
2023-12-07 12:46:31 +1100

    tf fmt

--- policies.tf
+++ policies.tf
@@ -1,4 +1,4 @@
 resource "vault_policy" "create_child_token" {
-	name = "create_child_token"
-	policy = file("./policies/create_child_token.hcl")
+  name   = "create_child_token"
+  policy = file("./policies/create_child_token.hcl")
 }
