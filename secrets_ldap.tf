982ea6c48b194096cec4fd6d3f4bdae40c07fb44
Grant Orchard <go@hashicorp.com>
2023-12-08 08:46:50 +1100

    move to ldaps for connection

--- secrets_ldap.tf
+++ secrets_ldap.tf
@@ -3,8 +3,7 @@ resource "vault_ldap_secret_backend" "config" {
   path         = var.ldap_path
   binddn       = var.ldap_binddn
   bindpass     = var.ldap_bindpass
-  url          = var.ldap_url
-  insecure_tls = var.ldap_insecure_tls
+  url          = "ldaps://hashicorp.local"
   userdn       = var.ldap_userdn
 
 }
