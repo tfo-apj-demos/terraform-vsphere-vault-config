6790eb2e258f99e93c7559ac4f86bae0f8c792d4
Grant Orchard <go@hashicorp.com>
2023-12-07 19:54:25 +1100

    trying to fix 'LDAP Result Code 34 Invalid DN Syntax'

--- secrets_ldap.tf
+++ secrets_ldap.tf
@@ -25,5 +25,5 @@ resource "vault_ldap_secret_backend_dynamic_role" "role" {
   rollback_ldif = file("${path.module}/files/rollback.ldif")
   default_ttl   = 300
   max_ttl       = 3600
-  username_template = "{{.RoleName}}-{{unix_time}}-{{random 8}}"
+  username_template = "{{.DisplayName | truncate 8 }}-{{unix_time}}-{{random 8}}"
 }
