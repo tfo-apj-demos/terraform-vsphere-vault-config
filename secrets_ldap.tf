9dc09e16e5a2f902d10d070c0adc865cb88cd93a
Grant Orchard <go@hashicorp.com>
2023-12-08 10:19:39 +1100

    move role to for_each

--- secrets_ldap.tf
+++ secrets_ldap.tf
@@ -1,35 +1,23 @@
 # --- Config for the LDAP secrets engine
 resource "vault_ldap_secret_backend" "this" {
-  path         = var.ldap_path
-  binddn       = var.ldap_binddn
-  bindpass     = var.ldap_bindpass
-  url          = "ldaps://dc-0.hashicorp.local" # have to provide the server here to match the certificate
-  userdn       = var.ldap_userdn
-  certificate  = file("${path.module}/ca_cert_dir/root_ca.pem")
+  path        = var.ldap_path
+  binddn      = var.ldap_binddn
+  bindpass    = var.ldap_bindpass
+  url         = "ldaps://dc-0.hashicorp.local" # have to provide the server here to match the certificate
+  userdn      = var.ldap_userdn
+  certificate = file("${path.module}/ca_cert_dir/root_ca.pem")
 }
 
-resource "vault_ldap_secret_backend_dynamic_role" "default" {
-  mount         = vault_ldap_secret_backend.this.path
-  role_name     = var.ldap_role_name
+resource "vault_ldap_secret_backend_dynamic_role" "this" {
+  for_each  = toset(var.ldap_roles)
+  mount     = vault_ldap_secret_backend.this.path
+  role_name = each.value.role_name
   creation_ldif = templatefile("${path.module}/files/creation.ldif.tmpl", {
-    group_names =  ["vault_dynamic"]
+    group_names = each.value.group_names
   })
-  deletion_ldif = file("${path.module}/files/deletion.ldif")
-  rollback_ldif = file("${path.module}/files/rollback.ldif")
-  default_ttl   = 3600   # One hour
-  max_ttl       = 8*3600 # Make it easy to see this is eight hours
-  username_template = "{{printf \"%s%s%s%s\" (.DisplayName | truncate 8) (.RoleName | truncate 8) (random 20)| truncate 20}}"
-}
-
-resource "vault_ldap_secret_backend_dynamic_role" "vsphere" {
-  mount         = vault_ldap_secret_backend.this.path
-  role_name     = "vsphere_virtual_machine_lifecycle"
-  creation_ldif = templatefile("${path.module}/files/creation.ldif.tmpl", {
-    group_names =  ["vsphere_virtual_machine_lifecycle"]
-  })
-  deletion_ldif = file("${path.module}/files/deletion.ldif")
-  rollback_ldif = file("${path.module}/files/rollback.ldif")
-  default_ttl   = 3600   # One hour
-  max_ttl       = 8*3600 # Make it easy to see this is eight hours
+  deletion_ldif     = file("${path.module}/files/deletion.ldif")
+  rollback_ldif     = file("${path.module}/files/rollback.ldif")
+  default_ttl       = 3600     # One hour
+  max_ttl           = 8 * 3600 # Make it easy to see this is eight hours
   username_template = "{{printf \"%s%s%s%s\" (.DisplayName | truncate 8) (.RoleName | truncate 8) (random 20)| truncate 20}}"
 }
--- variables.tf
+++ variables.tf
@@ -14,37 +14,17 @@ variable "ldap_bindpass" {
   sensitive   = true
 }
 
-variable "ldap_url" {
-  description = "URL for LDAP connection"
-  type        = string
-}
-
-variable "ldap_insecure_tls" {
-  description = "Flag to specify if TLS is insecure"
-  type        = bool
-}
-
 variable "ldap_userdn" {
   description = "DN of users for LDAP"
   type        = string
 }
 
-variable "ldap_role_name" {
-  description = "Name of the LDAP role"
-  type        = string
+variable "ldap_roles" {
+  description = "An object containing the role name and Active Directory groups to join."
+  type = list(object({
+    role_name   = string
+    group_names = list(string)
+  }))
 }
 
-variable "ldap_creation_ldif" {
-  description = "LDIF template used to create a user account"
-  type        = string
-}
 
-variable "ldap_deletion_ldif" {
-  description = "LDIF template used to delete a user account"
-  type        = string
-}
-
-variable "ldap_rollback_ldif" {
-  description = "LDIF template used to rollback changes on error"
-  type        = string
-}
