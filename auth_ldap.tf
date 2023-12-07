67966a25b2e5b2653876ded9abb4707aaba1cb87
Aaron Evans <aarone@outlook.com.au>
2023-12-07 12:19:36 +1100

    updated config location

--- auth_ldap.tf
+++ auth_ldap.tf
@@ -1,8 +1,1 @@
-resource "vault_ldap_secret_backend" "config" {
-  path          = var.ldap_path
-  binddn        = var.ldap_binddn
-  bindpass      = var.ldap_bindpass
-  url           = var.ldap_url
-  insecure_tls  = var.ldap_insecure_tls
-  userdn        = var.ldap_userdn
-}
+# --- Config for the LDAP auth for users
