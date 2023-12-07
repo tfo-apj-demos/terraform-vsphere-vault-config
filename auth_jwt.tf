c96975f3d682c07645f0e99f8e0babafaa962aa5
Aaron Evans <aarone@outlook.com.au>
2023-12-07 12:46:21 +1100

    tf fmt

--- auth_jwt.tf
+++ auth_jwt.tf
@@ -1,15 +1,15 @@
 # --- Config for JWT auth used by TFC
 
 resource "vault_jwt_auth_backend" "tfc" {
-    path                = "jwt"
-    oidc_discovery_url  = "https://app.terraform.io"
-    bound_issuer        = "https://app.terraform.io"
+  path               = "jwt"
+  oidc_discovery_url = "https://app.terraform.io"
+  bound_issuer       = "https://app.terraform.io"
 }
 
 resource "vault_jwt_auth_backend_role" "tfc" {
-  backend         = vault_jwt_auth_backend.tfc.path
-  role_name       = "tfc"
-  token_policies  = [
+  backend   = vault_jwt_auth_backend.tfc.path
+  role_name = "tfc"
+  token_policies = [
     "create_child_token"
   ]
 
@@ -18,14 +18,14 @@ resource "vault_jwt_auth_backend_role" "tfc" {
   ]
   bound_claims_type = "glob"
   bound_claims = {
-    sub = "organization:tfo-apj-demos:*"
+    sub                       = "organization:tfo-apj-demos:*"
     terraform_organization_id = "org-6nfrqkZhPPHJWG5h"
   }
-  user_claim      = "terraform_full_workspace"
-  role_type       = "jwt"
+  user_claim = "terraform_full_workspace"
+  role_type  = "jwt"
 
   claim_mappings = {
-    terraform_project_id = "terraform_project_id"
+    terraform_project_id   = "terraform_project_id"
     terraform_workspace_id = "terraform_workspace_id"
   }
 }
