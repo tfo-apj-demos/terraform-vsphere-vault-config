7a695b85e25913b78f89eeb5c524021b5023873a
Aaron Evans <aarone@outlook.com.au>
2023-12-08 21:46:42 +1100

    initalise ldap auth

--- auth_ldap.tf
+++ auth_ldap.tf
@@ -1,1 +1,10 @@
 # --- Config for the LDAP auth for users
+resource "vault_ldap_auth_backend" "ldap" {
+  path           = "ldap"
+  url            = var.ldap_url
+  userdn         = var.ldap_userdn
+  binddn         = var.ldap_binddn
+  bindpass       = var.ldap_bindpass
+  groupdn        = var.ldap_groupdn
+  token_policies = ["default"]
+}
