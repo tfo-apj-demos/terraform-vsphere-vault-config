1595487073c4b77fc64874ff5eac7739dbcbb101
Grant Orchard <go@hashicorp.com>
2023-12-11 13:23:55 +1100

    initial commit for oidc auth

--- auth_oidc.tf
+++ auth_oidc.tf
@@ -1,1 +1,26 @@
 # --- Config for OIDC auth for users
+
+# --- Config for OIDC auth for users
+
+resource "vault_jwt_auth_backend" "this" {
+  path               = "oidc"
+  type               = "oidc"
+  oidc_discovery_url = "https://production.vault.11eb56d6-0f95-3a99-a33c-0242ac110007.aws.hashicorp.cloud:8200/v1/admin/tfo-apj-demos/identity/oidc/provider/team_se"
+  oidc_client_id     = "Ty0hFqlMC8FYWpkz8znBeZxu0qz05lno"
+  oidc_client_secret = var.oidc_client_secret
+  default_role       = "systems_engineer"
+}
+
+resource "vault_jwt_auth_backend_role" "this" {
+  backend               = vault_jwt_auth_backend.this.path
+  role_name             = "systems_engineer"
+  token_policies        = [
+
+  ]
+  user_claim            = "email"
+  role_type             = "oidc"
+  allowed_redirect_uris = [
+    "http://localhost:8250/oidc/callback",
+    "https://vault.hashicorp.local:8200/ui/vault/auth/oidc/oidc/callback",
+  ]
+}
