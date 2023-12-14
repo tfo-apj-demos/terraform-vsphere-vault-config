c57d082fc272769ebc7d3d15cdaed4bc90c96bcd
Aaron Evans <aarone@outlook.com.au>
2023-12-14 21:40:25 +1100

    enable ldap secret engine

--- secrets_ldap.tf
+++ secrets_ldap.tf
@@ -24,3 +24,14 @@ resource "vault_password_policy" "active_directory" {
     }
   EOT
 }
+
+resource "vault_ldap_secret_backend" "this" {
+  path            = var.ldap_path
+  binddn          = var.ldap_binddn
+  bindpass        = var.ldap_bindpass
+  url             = var.ldap_url # have to provide the server here to match the certificate
+  userdn          = var.ldap_userdn
+  certificate     = file("${path.module}/ca_cert_dir/root_ca.pem")
+  password_policy = vault_password_policy.active_directory.id
+  schema          = "ad"
+}
