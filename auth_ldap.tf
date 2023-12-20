ce1bf0e6b5f32ebf18b3e7951c117b5b65c034d2
Aaron Evans <aarone@outlook.com.au>
2023-12-20 15:08:28 +1100

    adding ldap

--- auth_ldap.tf
+++ auth_ldap.tf
@@ -1,5 +1,5 @@
 # --- Config for the LDAP auth for users
-/*resource "vault_ldap_auth_backend" "ldap" {
+resource "vault_ldap_auth_backend" "ldap" {
   path           = "ldap"
   url            = var.ldap_url
   userdn         = var.ldap_userdn
@@ -7,4 +7,4 @@
   bindpass       = var.ldap_bindpass
   groupdn        = var.ldap_groupdn
   token_policies = ["default"]
-}*/
+}
