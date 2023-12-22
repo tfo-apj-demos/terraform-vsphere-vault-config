1073993c009160fa5bded5aea98309b523efb6bf
Grant Orchard <go@hashicorp.com>
2023-12-22 13:01:42 +1100

    untangle groups

--- auth_identity_entities.tf
+++ auth_identity_entities.tf
@@ -7,6 +7,7 @@ data "tfe_outputs" "github_identities" {
   workspace = var.tfc_workspace
 }
 
+# --- Create entities for the SEs, and aliases for OIDC auth.
 resource "vault_identity_entity_alias" "se" {
   for_each = vault_identity_entity.se
   name = each.value.name
@@ -19,24 +20,25 @@ resource "vault_identity_entity" "se" {
   name = each.value
 }
 
-resource "vault_identity_group" "team_se" {
-  type = "external"
-  name = "team-se"
-}
+# --- Create external groups based on OIDC group claims.
+# resource "vault_identity_group" "team_se" {
+#   type = "external"
+#   name = "team-se"
+# }
 
-resource "vault_identity_group_alias" "team_se" {
-  name           = "team-se"
-  mount_accessor = vault_jwt_auth_backend.this.accessor
-  canonical_id   = vault_identity_group.team_se.id
-}
+# resource "vault_identity_group_alias" "team_se" {
+#   name           = "team-se"
+#   mount_accessor = vault_jwt_auth_backend.this.accessor
+#   canonical_id   = vault_identity_group.team_se.id
+# }
 
-resource "vault_identity_group" "gcve_admins" {
-  type = "external"
-  name = "gcve-admins"
-}
+# resource "vault_identity_group" "gcve_admins" {
+#   type = "external"
+#   name = "gcve-admins"
+# }
 
-resource "vault_identity_group_alias" "gcve_admins" {
-  name           = "gcve-admins"
-  mount_accessor = vault_jwt_auth_backend.this.accessor
-  canonical_id   = vault_identity_group.gcve_admins.id
-}
+# resource "vault_identity_group_alias" "gcve_admins" {
+#   name           = "gcve-admins"
+#   mount_accessor = vault_jwt_auth_backend.this.accessor
+#   canonical_id   = vault_identity_group.gcve_admins.id
+# }
