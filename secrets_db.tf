d378f7c9a9374401d15d194c26983cb06998f969
Grant Orchard <go@hashicorp.com>
2024-02-02 15:57:35 +1100

    recreate moutn

--- secrets_db.tf
+++ secrets_db.tf
@@ -1,5 +1,6 @@
-resource "vault_database_secrets_mount" "postgres" {
+resource "vault_mount" "postgres" {
   path = "postgres"
+  type = "database"
 }
 
 # --- Note that all connections and roles are configured through self service.
