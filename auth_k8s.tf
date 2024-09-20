721cd21c770686e43561a3820649ca2a28ad5076
Aaron Evans <aarone@outlook.com.au>
2024-09-20 11:23:13 +1000

    Decode Kubernetes CA certificate before use in authentication backend configuration

--- auth_k8s.tf
+++ auth_k8s.tf
@@ -5,7 +5,7 @@ resource "vault_auth_backend" "this" {
 resource "vault_kubernetes_auth_backend_config" "this" {
   backend            = vault_auth_backend.this.path
   kubernetes_host    = var.kubernetes_api
-  kubernetes_ca_cert = var.kubernetes_ca_cert
+  kubernetes_ca_cert = base64decode(var.kubernetes_ca_cert)
   token_reviewer_jwt = var.token_reviewer_jwt
 }
 
