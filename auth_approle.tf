050cef4ed6e4375e884045d1d92033ab1466f8a1
Aaron Evans <aarone@outlook.com.au>
2024-11-11 09:41:18 +1100

    Add 'update_approle' policy to Ansible AppRole for enhanced permissions

--- auth_approle.tf
+++ auth_approle.tf
@@ -6,7 +6,7 @@ resource "vault_auth_backend" "approle" {
 resource "vault_approle_auth_backend_role" "ansible" {
   backend         = vault_auth_backend.approle.path
   role_name       = "ansible"
-  token_policies  = ["sign_ssh"]
+  token_policies  = ["sign_ssh", "update_approle" ]
 }
 
 resource "vault_approle_auth_backend_role_secret_id" "id" {
