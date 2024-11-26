18c5b82c087819aab62a593111db60edb611ff49
Aaron Evans <aarone@outlook.com.au>
2024-11-26 14:39:04 +1100

    Remove unused KV mounts and secrets from secrets_kv.tf

--- secrets_kv.tf
+++ secrets_kv.tf
@@ -3,27 +3,3 @@ resource "vault_mount" "this" {
   type        = "kv"
   options     = { version = "2" }
 }
-
-resource "vault_mount" "hostnaming" {
-  path        = "hostnaming"
-  type        = "kv"
-  options     = { version = "2" }
-  description = "Key/Value mount for generating hostnames for virtual machines"
-}
-
-resource "vault_mount" "djoo-kv-demo" {
-  path        = "kv-demo"
-  type        = "kv"
-  options     = { version = "2" }
-  description = "djoo kv demo"
-}
-
-resource "vault_kv_secret_v2" "djoo_secret" {
-  mount = vault_mount.djoo-kv-demo.path
-  name  = "djoo"
-  data_json = jsonencode(
-    {
-      api_cred = "aaaa-bbbb-cccc-dddd"
-    }
-  )
-}
