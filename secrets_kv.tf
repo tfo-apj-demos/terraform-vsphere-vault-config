0def0080c4b5816dbb07ba7abe7a6fab583feba3
Aaron Evans <aarone@outlook.com.au>
2024-11-26 14:44:56 +1100

    Fix KV secret mount path in secrets_kv.tf

--- secrets_kv.tf
+++ secrets_kv.tf
@@ -4,7 +4,7 @@ resource "vault_mount" "this" {
   options     = { version = "2" }
 }
 resource "vault_kv_secret_v2" "api_creds" {
-  mount = vault_mount.djoo-kv-demo.path
+  mount = vault_mount.this.path
   name  = "dev"
   data_json = jsonencode(
     {
