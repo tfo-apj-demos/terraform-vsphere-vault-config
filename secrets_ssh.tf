bdcdab10cce13adad4793a9222f8972017210616
Aaron Evans <aarone@outlook.com.au>
2025-04-28 16:34:52 +1000

    Refactor SSH roles: rename 'ansible-ubuntu' to 'ansible' and remove 'ansible-rhel' role

--- secrets_ssh.tf
+++ secrets_ssh.tf
@@ -8,25 +8,11 @@ resource "vault_ssh_secret_backend_ca" "this" {
 		generate_signing_key = true
 }
 
-resource "vault_ssh_secret_backend_role" "ansible-ubuntu" {
+resource "vault_ssh_secret_backend_role" "this" {
   backend = vault_mount.ssh.path
-	name = "ansible-ubuntu"
+	name = "ansible"
 	allow_user_certificates = true
   default_user = "ubuntu"
-  allowed_users = "*"
-  key_type = "ca"
-  ttl = "28800"
-  max_ttl = "28800"
-  default_extensions = {"permit-pty"=""}
-  allowed_extensions = "permit-pty,permit-port-forwarding"
-}
-
-# new role for ansible ssh into rhel machine with a local user called vm_user
-resource "vault_ssh_secret_backend_role" "ansible-rhel" {
-  backend = vault_mount.ssh.path
-  name = "ansible-rhel"
-  allow_user_certificates = true
-  default_user = "vm_user"
   allowed_users = "*"
   key_type = "ca"
   ttl = "28800"
