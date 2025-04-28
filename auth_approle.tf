fac04d92aa648e35798e78e134c87bd7adbbd726
Aaron Evans <aarone@outlook.com.au>
2025-04-28 16:54:38 +1000

    Uncomment Ansible approle role and outputs for proper configuration

--- auth_approle.tf
+++ auth_approle.tf
@@ -2,14 +2,14 @@ resource "vault_auth_backend" "approle" {
   type = "approle"
 }
 
-# # Set up the approle role for Ansible Automation Platform
-# resource "vault_approle_auth_backend_role" "ansible" {
-#   backend         = vault_auth_backend.approle.path
-#   role_name       = "ansible"
-#   token_policies  = ["sign_ssh", "update_approle", "read_kv"]
-# }
+# Set up the approle role for Ansible Automation Platform
+resource "vault_approle_auth_backend_role" "ansible" {
+  backend         = vault_auth_backend.approle.path
+  role_name       = "ansible"
+  token_policies  = ["sign_ssh", "update_approle", "read_kv"]
+}
 
-# resource "vault_approle_auth_backend_role_secret_id" "id" {
-#   backend   = vault_auth_backend.approle.path
-#   role_name = vault_approle_auth_backend_role.ansible.role_name
-# }
+resource "vault_approle_auth_backend_role_secret_id" "id" {
+  backend   = vault_auth_backend.approle.path
+  role_name = vault_approle_auth_backend_role.ansible.role_name
+}
--- outputs.tf
+++ outputs.tf
@@ -3,12 +3,12 @@ output "ca_chain_certificate" {
   description = "The content of the CA certificate."
 }
 
-# output "ansible_secret_id" {
-#   value       = nonsensitive(vault_approle_auth_backend_role_secret_id.id.secret_id)
-#   description = "The secret_id for the approle ansible."
-# }
+output "ansible_secret_id" {
+  value       = nonsensitive(vault_approle_auth_backend_role_secret_id.id.secret_id)
+  description = "The secret_id for the approle ansible."
+}
 
-# output "ansible_role_id" {
-#   value       = nonsensitive(vault_approle_auth_backend_role.ansible.role_id)
-#   description = "The role_id for the approle ansible."
-# }
+output "ansible_role_id" {
+  value       = nonsensitive(vault_approle_auth_backend_role.ansible.role_id)
+  description = "The role_id for the approle ansible."
+}
