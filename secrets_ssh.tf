339e86bacaf2aa11b2b92ef146406aed32ba1679
Aaron Evans <aarone@outlook.com.au>
2025-04-28 16:36:55 +1000

    Rename SSH role to 'ansible-ubuntu' and add new role 'ansible-rhel' for SSH access with local user 'vm_user'

--- secrets_ssh.tf
+++ secrets_ssh.tf
@@ -8,11 +8,25 @@ resource "vault_ssh_secret_backend_ca" "this" {
 		generate_signing_key = true
 }
 
-resource "vault_ssh_secret_backend_role" "this" {
+resource "vault_ssh_secret_backend_role" "ansible-ubuntu" {
   backend = vault_mount.ssh.path
-	name = "ansible"
+	name = "ansible-ubuntu"
 	allow_user_certificates = true
   default_user = "ubuntu"
+  allowed_users = "*"
+  key_type = "ca"
+  ttl = "28800"
+  max_ttl = "28800"
+  default_extensions = {"permit-pty"=""}
+  allowed_extensions = "permit-pty,permit-port-forwarding"
+}
+
+# new role for ansible ssh into rhel machine with a local user called vm_user
+resource "vault_ssh_secret_backend_role" "ansible-rhel" {
+  backend = vault_mount.ssh.path
+  name = "ansible-rhel"
+  allow_user_certificates = true
+  default_user = "vm_user"
   allowed_users = "*"
   key_type = "ca"
   ttl = "28800"
