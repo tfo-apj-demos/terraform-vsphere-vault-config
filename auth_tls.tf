da020c5bf718904e0db0805850578e3122822755
Aaron Evans <aarone@outlook.com.au>
2025-04-30 22:26:59 +1000

    Update certificate references to use intermediate certificate for client cert authentication

--- auth_tls.tf
+++ auth_tls.tf
@@ -3,14 +3,14 @@ resource "vault_auth_backend" "cert" {
   path = "cert"
 }
 
-data "local_file" "root_ca_cert" {
-  filename = "${path.root}/ca_cert_dir/ca_chain.pem"
+data "local_file" "intermediate_cert" {
+  filename = "${path.root}/ca_cert_dir/intermediate.pem"
 }
 
 resource "vault_cert_auth_backend_role" "client-cert-auth" {
   backend              = vault_auth_backend.cert.path
   name                 = "client-cert-auth"
   display_name         = "Client Cert Auth Role"
-  certificate          = data.local_file.root_ca_cert.content
+  certificate          = data.local_file.intermediate_cert.content
   allowed_common_names = ["*"]
 }
