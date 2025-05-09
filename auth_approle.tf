4ea0edbac8a4cbd76f00c1074dde124de21d9f1a
Aaron Evans <aarone@outlook.com.au>
2025-05-10 00:43:48 +1000

    Add create_pki to token policies for Ansible approle

--- auth_approle.tf
+++ auth_approle.tf
@@ -6,7 +6,7 @@ resource "vault_auth_backend" "approle" {
 resource "vault_approle_auth_backend_role" "ansible" {
   backend         = vault_auth_backend.approle.path
   role_name       = "ansible"
-  token_policies  = ["sign_ssh", "update_approle", "read_kv"]
+  token_policies  = ["sign_ssh", "update_approle", "read_kv", "create_pki"]
 }
 
 resource "vault_approle_auth_backend_role_secret_id" "id" {
