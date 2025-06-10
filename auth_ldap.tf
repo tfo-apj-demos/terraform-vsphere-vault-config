b09de574049ddd035ae153e5b403a40155dfa4ad
Aaron Evans <aarone@outlook.com.au>
2025-06-10 16:02:29 +1000

    Restore LDAP auth backend configuration by uncommenting the resource block

--- auth_ldap.tf
+++ auth_ldap.tf
@@ -1,11 +1,11 @@
-# # --- Config for the LDAP auth for users
-# resource "vault_ldap_auth_backend" "ldap" {
-#   path           = var.ldap_path
-#   binddn         = var.ldap_binddn
-#   bindpass       = var.ldap_bindpass
-#   url            = var.ldap_url
-#   userdn         = var.ldap_userdn
-#   groupdn        = var.ldap_groupdn
-#   certificate    = file("${path.module}/ca_cert_dir/root_ca.pem")
-#   token_policies = ["default"]
-# }
+# --- Config for the LDAP auth for users
+resource "vault_ldap_auth_backend" "ldap" {
+  path           = var.ldap_path
+  binddn         = var.ldap_binddn
+  bindpass       = var.ldap_bindpass
+  url            = var.ldap_url
+  userdn         = var.ldap_userdn
+  groupdn        = var.ldap_groupdn
+  certificate    = file("${path.module}/ca_cert_dir/root_ca.pem")
+  token_policies = ["default"]
+}
